e70bd11ec624839aeb9090509fd86de7f0a5054a
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-08 17:45:29 +0100

    creating the store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "increment") {
+        return {
+            counter: state.counter + 1,
+        };
+    }
+
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1,
+        };
+    }
+};
+
+const store = redux.createStore(counterReducer);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
