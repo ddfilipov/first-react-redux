e7d984413a7eb4b20c765e7acc078f0bb91e1561
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-08 18:03:51 +0100

    added two new buttons and a useDispatch redux hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,20 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import store from "../store/index.js";
 
 const Counter = () => {
     const counter = useSelector((state) => state.counter);
-    
+
     const toggleCounterHandler = () => {};
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
             <div className={classes.value}>{counter}</div>
+            <div>
+                <button>Incremente</button>
+                <button>Decrement</button>
+            </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
     );
